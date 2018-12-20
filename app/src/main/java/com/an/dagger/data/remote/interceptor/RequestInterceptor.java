95811cb56e2f4a3ea37ad2a02e6c11c2ae3a56db
anitaa <anitaa@airloyal.com>
2018-12-20 20:01:34 +0530

    #4 - Configuring Retrofit api service in java

---
+++ app/src/main/java/com/an/dagger/data/remote/interceptor/RequestInterceptor.java
@@ -0,0 +1,25 @@
+package com.an.dagger.data.remote.interceptor;
+
+import com.an.dagger.BuildConfig;
+
+import java.io.IOException;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RequestInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request originalRequest = chain.request();
+        HttpUrl originalUrl = originalRequest.url();
+        HttpUrl url = originalUrl.newBuilder()
+                .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
+                .build();
+
+        Request.Builder requestBuilder = originalRequest.newBuilder().url(url);
+        Request request = requestBuilder.build();
+        return chain.proceed(request);
+    }
+}
---
+++ app/src/main/java/com/an/dagger/data/remote/model/MovieApiResponse.java
@@ -0,0 +1,55 @@
+package com.an.dagger.data.remote.model;
+
+import com.an.dagger.data.local.entity.MovieEntity;
+import com.google.gson.annotations.SerializedName;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieApiResponse {
+
+    public MovieApiResponse() {
+        this.results = new ArrayList<>();
+    }
+
+    private long page;
+
+    @SerializedName("total_pages")
+    private long totalPages;
+
+    @SerializedName("total_results")
+    private long totalResults;
+
+    private List<MovieEntity> results;
+
+    public long getPage() {
+        return page;
+    }
+
+    public void setPage(long page) {
+        this.page = page;
+    }
+
+    public long getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(long totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public long getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(long totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<MovieEntity> getResults() {
+        return results;
+    }
+
+    public void setResults(List<MovieEntity> results) {
+        this.results = results;
+    }
+}
